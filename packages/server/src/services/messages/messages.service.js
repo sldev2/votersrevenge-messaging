45753e65f021cc3f11cb8babd1f3a4da069fea62
Mudassar <mudsr3@gmail.com>
2021-09-06 23:22:11 +0500

    installed mongo db + now working on serices and models compatibility with new db

--- packages/server/src/services/messages/messages.service.js
+++ packages/server/src/services/messages/messages.service.js
@@ -12,6 +12,17 @@ module.exports = function (app) {
   // Initialize our service with any options it requires
   app.use('/messages', new Messages(options, app))
 
+  // // Connect to your MongoDB instance(s)
+  // MongoClient.connect('mongodb://localhost:27017/feathers').then(function(db){
+  //   // Connect to the db, create and register a Feathers service.
+  //   app.use('/messages', service({
+  //     Model: db.collection('messages'),
+  //     paginate: {
+  //       default: 2,
+  //       max: 4
+  //     }
+  // }));
+
   // Get our initialized service so that we can register hooks
   const service = app.service('messages')
 
@@ -20,4 +31,5 @@ module.exports = function (app) {
   service.publish((data, context) => {
     return app.channel(`room/${data.room}`)
   })
+
 }
